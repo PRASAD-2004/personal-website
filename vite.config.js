349b8b534a7f21debe740b9a13711af742b4b3ec
PRASAD-2004 <21951A0519@iare.ac.in>
2024-01-11 21:57:35 +0530

    final commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: "/my_personal_webpage/",
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  base: "/personal-websitegit/",
   plugins: [react()],
 })
